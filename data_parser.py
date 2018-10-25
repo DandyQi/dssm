acccf21b9aca8b95fca847df8cc0a055f31e5f1f
xza <10247854@qq.com>
2018-10-25 13:30:52 +0800

    l

--- data_parser.py
+++ data_parser.py
@@ -6,6 +6,11 @@ import random
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+import string
+from sklearn.metrics.pairwise import pairwise_distances
+import math
+from nltk.util import ngrams
+from itertools import permutations,groupby
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
@@ -42,7 +47,7 @@ def make_author_pair(sub, obj):
 
     common_author_list = pd.merge(left=sub_author_list, right=obj_author_list, how="inner", on="author")["author"]
     logger.info("the size of collaboration author: %s" % len(common_author_list))
-
+    
     pair = []
     for author in common_author_list:
         paper_list = author2paper[author2paper["author"] == author]["paper_idx"].values.astype(np.int32)
@@ -113,6 +118,129 @@ def concat(row):
     return row
 
 
+def word_count(file_list):
+    import collections
+    word_freq = collections.defaultdict(int)
+    for file in file_list:
+        with open(file) as f:
+            for l in f:
+                for w in l.strip().split():  
+                    word_freq[w] += 1
+    return word_freq
+
+def build_dict(file_name, min_word_freq=50):
+    word_freq = word_count(file_name) 
+   # word_freq = filter(lambda x: x[1] > min_word_freq, word_freq.items()) # filter将词频数量低于指定值的单词删除。
+    word_freq_sorted = sorted(word_freq, key=lambda x: (-x[1], x[0]))
+    # key用于指定排序的元素，因为sorted默认使用list中每个item的第一个元素从小到
+    #大排列，所以这里通过lambda进行前后元素调序，并对词频去相反数，从而将词频最大的排列在最前面
+    words, _ = list(zip(*word_freq_sorted))
+    word_idx = dict(zip(words, xrange(len(words))))
+    word_idx['<unk>'] = len(words) #unk表示unknown，未知单词
+    return word_idx
+
+# def deleteDuplicatedElementFromList3(listA):
+#     # return list(set(listA))
+#     return sorted(set(listA), key=listA.index)
+
+def deleteDuplicatedElementFromList3(l3g):
+    new_list = []
+    l3g.sort()
+    l3g = groupby(l3g)
+    for k, m in l3g:
+        new_list.extend(k)
+    return sorted(set(new_list), key=new_list.index)
+def word_to_ngrams(word,ngram_size=3,lower=True):
+    """Returns a list of all n-gram possibilities of the given word."""
+    if lower:
+        word = word.lower()
+    word = '<' + word + '>'
+    return list(map(lambda x: ''.join(x), list(ngrams(word,ngram_size))))
+
+def letter3gramtable(corpus):
+    l3g = []
+    for line in corpus:
+        remove_punct_map = dict.fromkeys(map(ord, string.punctuation))
+        #去除标点符号
+        line = line.translate(remove_punct_map)
+        for word in line.split():
+            #print(word)
+            w2n = word_to_ngrams(word)
+            l3g.extend(w2n)
+    print('Before remove duplication,length:',len(l3g))
+    l3g = deleteDuplicatedElementFromList3(l3g)
+    print('Remove duplication,length:',len(l3g))
+    return l3g
+
+# def letter3gramtable():
+#     vocabulary = ['a','b','c','d','e','f','g','h','i','j','k','l','m',\
+#                     'n','o','p','q','r','s','t','u','v','w','x','y','z']
+#     w2n_3 = list(permutations(vocabulary,3))
+#     l3g = []
+#     for i in w2n_3:
+#         temp = "".join(list(i))
+#         l3g.append(temp)
+#     w2n_2 = list(permutations(vocabulary,2))
+#     l2g = []
+#     for i in w2n_2:
+#         temp = "".join(list(i))
+#         l2g.append(temp)
+#     l2g_left =[]
+#     l2g_right = []
+#     for i in l2g:
+#         l2g_left.append('<' + i) 
+#     for i in l2g:
+#         l2g_right.append(i + '>')
+#     l3g.extend(l2g_left)
+#     l3g.extend(l2g_right)
+#     return l3g
+
+def letter3grams(corpus):
+    l3g = letter3gramtable(corpus)
+    feature = np.zeros((len(corpus),len(l3g)))
+    i = 0
+    for line in corpus:
+        if i % 100 == 0:
+            print('Transformed lines:%d' %i)
+        remove_punct_map = dict.fromkeys(map(ord, string.punctuation))
+        #去除标点符号
+        line = line.translate(remove_punct_map)
+        line_l3g_count = np.zeros(len(l3g), dtype='int')
+        for word in line.split():
+            word_l3g_count = np.zeros(len(l3g), dtype='int')
+            w3g = word_to_ngrams(word)
+            for j in w3g:
+                # if j in l3g:
+                    idx = l3g.index(j)
+                    word_l3g_count[idx] += 1
+
+            # print (V1)
+            line_l3g_count += word_l3g_count
+        
+        feature[i] = line_l3g_count
+        i += 1
+    return feature
+
+def lsh_transform(feature,k = 2000):
+    # feature = feature.todense()
+
+    trans_mat = np.random.randn(feature.shape[1],k)
+    hash_mat = np.dot(feature,trans_mat)
+    hash_mat = np.array(hash_mat)
+    for i in range(len(hash_mat)):
+        for j in range(k):
+            if hash_mat[i][j] >= 0. :
+                hash_mat[i][j] = 1
+            else:
+                hash_mat[i][j] = 0
+    return hash_mat
+
+def lsh_cosine(feature):
+    hash_mat = lsh_transform(feature)
+    hamming_distance = len(np.nonzero(hash_mat[17]-hash_mat[19])[0])
+    cos_distance = math.cos(hamming_distance/len(hash_mat[0])*math.pi)
+    return cos_distance
+
 if __name__ == "__main__":
     file_list = [
         "Cross-Domain_data/Data Mining.txt",
@@ -145,7 +273,19 @@ if __name__ == "__main__":
     author_pair = make_author_pair(sub_view, obj_view)
 
     corpus = data_set["content"].values
-    vectorizer = TfidfVectorizer()
-    feature = vectorizer.fit_transform(corpus)
+    # print(corpus.split(","))
+    # vectorizer = TfidfVectorizer()
+    # feature = vectorizer.fit_transform(corpus)
+    # print(feature.shape)
+    # make_training_set(author_pair, train_test_year)
+    # with open('./Cross-Domain_data/corpus.txt','a') as f:
+    #     for content in corpus:
+    #         f.write(str(content)+'\r\n')
+
+    feature = letter3grams(corpus)
     print(feature.shape)
+    print(feature[0])
+    
+    # print(1-pairwise_distances(feature[0:20],metric="cosine")[17][19])
+    # print(lsh_cosine(feature))
     # make_training_set(author_pair, train_test_year)
