8bdcb90e777f71d3491ac19fe35fff2c841a2e42
xza <10247854@qq.com>
2018-10-30 10:20:21 +0800

    l

--- data_parser.py
+++ data_parser.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import os
 import logging
 import pickle
 import random
@@ -11,7 +11,7 @@ from sklearn.metrics.pairwise import pairwise_distances
 import math
 from nltk.util import ngrams
 from itertools import permutations,groupby
-
+import h5py
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 logger = logging.getLogger("data_parser")
@@ -66,7 +66,7 @@ def make_feature(author):
     papers = author2paper[author2paper["author"] == author]
     papers["year"] = papers["year"].astype(np.int32)
     paper_list = papers[papers["year"] <= train_test_year]["paper_idx"].values.astype(np.int32)
-    feat = np.mean(feature[paper_list].toarray(), axis=0)
+    feat = np.mean(feature[paper_list], axis=0)
     return feat
 
 
@@ -139,17 +139,10 @@ def build_dict(file_name, min_word_freq=50):
     word_idx['<unk>'] = len(words) #unk表示unknown，未知单词
     return word_idx
 
-# def deleteDuplicatedElementFromList3(listA):
-#     # return list(set(listA))
-#     return sorted(set(listA), key=listA.index)
+def deleteDuplicatedElementFromList3(listA):
+    # return list(set(listA))
+    return sorted(set(listA), key=listA.index)
 
-def deleteDuplicatedElementFromList3(l3g):
-    new_list = []
-    l3g.sort()
-    l3g = groupby(l3g)
-    for k, m in l3g:
-        new_list.extend(k)
-    return sorted(set(new_list), key=new_list.index)
 def word_to_ngrams(word,ngram_size=3,lower=True):
     """Returns a list of all n-gram possibilities of the given word."""
     if lower:
@@ -159,41 +152,25 @@ def word_to_ngrams(word,ngram_size=3,lower=True):
 
 def letter3gramtable(corpus):
     l3g = []
+    i = 0
     for line in corpus:
+        if i % 100 ==0:
+            print('Generating line:%d' % i)
         remove_punct_map = dict.fromkeys(map(ord, string.punctuation))
         #去除标点符号
         line = line.translate(remove_punct_map)
         for word in line.split():
             #print(word)
             w2n = word_to_ngrams(word)
+
             l3g.extend(w2n)
+        i += 1
     print('Before remove duplication,length:',len(l3g))
     l3g = deleteDuplicatedElementFromList3(l3g)
-    print('Remove duplication,length:',len(l3g))
+    print('Removed duplication,length:',len(l3g))
     return l3g
 
-# def letter3gramtable():
-#     vocabulary = ['a','b','c','d','e','f','g','h','i','j','k','l','m',\
-#                     'n','o','p','q','r','s','t','u','v','w','x','y','z']
-#     w2n_3 = list(permutations(vocabulary,3))
-#     l3g = []
-#     for i in w2n_3:
-#         temp = "".join(list(i))
-#         l3g.append(temp)
-#     w2n_2 = list(permutations(vocabulary,2))
-#     l2g = []
-#     for i in w2n_2:
-#         temp = "".join(list(i))
-#         l2g.append(temp)
-#     l2g_left =[]
-#     l2g_right = []
-#     for i in l2g:
-#         l2g_left.append('<' + i) 
-#     for i in l2g:
-#         l2g_right.append(i + '>')
-#     l3g.extend(l2g_left)
-#     l3g.extend(l2g_right)
-#     return l3g
+
 
 def letter3grams(corpus):
     l3g = letter3gramtable(corpus)
@@ -201,7 +178,7 @@ def letter3grams(corpus):
     i = 0
     for line in corpus:
         if i % 100 == 0:
-            print('Transformed lines:%d' %i)
+            print('Transforming lines:%d' %i)
         remove_punct_map = dict.fromkeys(map(ord, string.punctuation))
         #去除标点符号
         line = line.translate(remove_punct_map)
@@ -219,26 +196,30 @@ def letter3grams(corpus):
         
         feature[i] = line_l3g_count
         i += 1
+    print('Writing file: feature.h5')
+    file = h5py.File('./Cross-Domain_data/feature.h5','w')
+    file.create_dataset('A',data = feature)
+    file.close()
     return feature
 
 def lsh_transform(feature,k = 2000):
     # feature = feature.todense()
 
     trans_mat = np.random.randn(feature.shape[1],k)
-    hash_mat = np.dot(feature,trans_mat)
-    hash_mat = np.array(hash_mat)
-    for i in range(len(hash_mat)):
+    hashed_feature = np.dot(feature,trans_mat)
+    hashed_feature = np.array(hashed_feature)
+    for i in range(len(hashed_feature)):
         for j in range(k):
-            if hash_mat[i][j] >= 0. :
-                hash_mat[i][j] = 1
+            if hashed_feature[i][j] >= 0. :
+                hashed_feature[i][j] = 1
             else:
-                hash_mat[i][j] = 0
-    return hash_mat
+                hashed_feature[i][j] = 0
+    return hashed_feature
 
 def lsh_cosine(feature):
-    hash_mat = lsh_transform(feature)
-    hamming_distance = len(np.nonzero(hash_mat[17]-hash_mat[19])[0])
-    cos_distance = math.cos(hamming_distance/len(hash_mat[0])*math.pi)
+    hashed_feature = lsh_transform(feature)
+    hamming_distance = len(np.nonzero(hashed_feature[17]-hashed_feature[19])[0])
+    cos_distance = math.cos(hamming_distance/len(hashed_feature[0])*math.pi)
     return cos_distance
 
 if __name__ == "__main__":
@@ -249,42 +230,40 @@ if __name__ == "__main__":
         "Cross-Domain_data/Theory.txt",
         "Cross-Domain_data/Visualization.txt"
     ]
+    if os.path.exists('./Cross-Domain_data/feature.h5'):
+        print('Reading file: feature.h5')
+        f = h5py.File('./Cross-Domain_data/feature.h5','r')
+        feature = f['A'][:]
+        f.close()
+    else:
+        train_test_year = 2001
 
-    train_test_year = 2001
+        df_list = []
+        for file in file_list:
+            df = pd.read_csv(file, sep="\t", names=["conference/journal", "title", "author", "year", "abstract"]).dropna()
+            df["domain"] = file.split("/")[1].split(".")[0]
+            df_list.append(df)
+        data_set = pd.concat(df_list, ignore_index=True)
+        data_set = pd.DataFrame(data_set)
 
-    df_list = []
-    for file in file_list:
-        df = pd.read_csv(file, sep="\t", names=["conference/journal", "title", "author", "year", "abstract"]).dropna()
-        df["domain"] = file.split("/")[1].split(".")[0]
-        df_list.append(df)
-    data_set = pd.concat(df_list, ignore_index=True)
-    data_set = pd.DataFrame(data_set)
+        data_set = data_set.apply(year_str2int, axis=1)
+        data_set = data_set.apply(concat, axis=1)
 
-    data_set = data_set.apply(year_str2int, axis=1)
-    data_set = data_set.apply(concat, axis=1)
+        logger.info("the size of data set: %s" % data_set.shape[0])
+        author2paper = make_paper_mapping(data_set)
+        domain2author = make_domain_mapping(data_set)
 
-    logger.info("the size of data set: %s" % data_set.shape[0])
-    author2paper = make_paper_mapping(data_set)
-    domain2author = make_domain_mapping(data_set)
+        sub_view = "Data Mining"
+        obj_view = "Theory"
 
-    sub_view = "Data Mining"
-    obj_view = "Theory"
+        author_pair = make_author_pair(sub_view, obj_view)
 
-    author_pair = make_author_pair(sub_view, obj_view)
-
-    corpus = data_set["content"].values
-    # print(corpus.split(","))
-    # vectorizer = TfidfVectorizer()
-    # feature = vectorizer.fit_transform(corpus)
-    # print(feature.shape)
-    # make_training_set(author_pair, train_test_year)
-    # with open('./Cross-Domain_data/corpus.txt','a') as f:
-    #     for content in corpus:
-    #         f.write(str(content)+'\r\n')
-
-    feature = letter3grams(corpus)
+        corpus = data_set["content"].values
+        # print(corpus.split(","))
+        # vectorizer = TfidfVectorizer()
+        # feature = vectorizer.fit_transform(corpus)
+        feature = letter3grams(corpus)
     print(feature.shape)
-    print(feature[0])
     
     # print(1-pairwise_distances(feature[0:20],metric="cosine")[17][19])
     # print(lsh_cosine(feature))
